c8ce7ba02c81ee11ca605dc3af730e07f4eadf95
DDachev <dacho90@abv.bg>
2020-02-06 16:59:55 +0200

    Task0-Lecture04

---
+++ Task0.java
@@ -0,0 +1,57 @@
+package bg.swift.HW04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task0 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		Task0a_CreateArray
+
+//		int [] shuffledNumbers = {5, 9, 11, 3, 6, 4, 7};
+//		for(int el : shuffledNumbers) {
+//			System.out.println(el);
+//		}
+		
+//		Task0b_ReadArray
+		
+//		int countElements = sc.nextInt();
+//		int [] array = new int [countElements];
+//		for(int i = 0; i < array.length; i++) {
+//			array [i] = Integer.parseInt(sc.next());
+//		}
+//		for(int i = 0; i < array.length; i++) {
+//			System.out.print(array[i]);
+//			if (i < array.length-1) {
+//				System.out.print(",");
+//			}
+//		}
+		
+//		Task0c_PrintString
+		
+//		String line = sc.nextLine();
+//		for(int i = 0; i < line.length(); i++) {
+//			System.out.println(line.charAt(i));
+//		}
+		
+//		Task0d_Print2dArray
+		
+//		int matrix [][] = new int [4][4];
+//		int count = 1;
+//		for (int i = 0; i < matrix.length; i++) {
+//			for (int j = 0; j < matrix.length; j++) {
+//				matrix[i][j] = count;
+//				count++;
+//			}
+//		}
+//		for (int i = 0; i < matrix.length; i++) {
+//			for (int j = 0; j < matrix.length; j++) {
+//				System.out.print(matrix[i][j] + "  ");
+//			}
+//			System.out.println();
+//		}
+		
+	}
+}
